c7d0afe3c888843cedc14f0492859011f99e6d8e
Connor Nelson <connornelson@Connors-Mac.local>
2014-03-19 13:22:33 -0400

    Initial Commit

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Connor Nelson on 3/19/14.
+//  Copyright (c) 2014 Connor Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello World/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Hello World
+//
+//  Created by Connor Nelson on 3/19/14.
+//  Copyright (c) 2014 Connor Nelson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
